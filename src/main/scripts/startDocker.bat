e9d600fe1484bf71e31909d5953af751ebd554b8
Hoang Son Nguyen <Hoang.Son.Nguyen@anecon.com>
2019-01-06 22:54:42 +0100

    docker 2.

--- src/main/scripts/startDocker.bat
+++ src/main/scripts/startDocker.bat
@@ -2,3 +2,4 @@ cd C:\Users\Hoang.Son.Nguyen\OneDrive\Public\masteringseleniumtestng\src\main\sc
 docker run -d -p 4444:4444 --name selenium-hub selenium/hub
 docker run -d --link selenium-hub:hub selenium/node-chrome
 docker run -d --link selenium-hub:hub selenium/node-firefox
+
